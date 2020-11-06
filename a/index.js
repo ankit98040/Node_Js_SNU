5004bdc1a5d530fbe14817da21eaaf6bd7cd6f5b
ankit <pramanik.85849@gmail.com>
2020-11-06 13:42:23 +0530

    updates

---
+++ a/index.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+
+var host = '127.0.0.1';
+var port = '3500';
+
+//ip -> 127.0.0.1:3500
+
+fs.readFile('./index.html', function(err, html){
+	if(err){
+		console.log(err);
+		return;
+	}
+
+	var server = http.createServer(function(req, res){
+		res.statusCode = 200;
+		res.setHeader('Content-type', 'text/html');
+		res.write(html);
+		res.end();
+	});
+
+	server.listen(port, host, function(){
+		console.log('Server running at port: ' +port);
+	});
+
+});
+
+
